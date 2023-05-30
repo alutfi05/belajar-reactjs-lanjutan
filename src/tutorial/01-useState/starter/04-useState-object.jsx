0163a2f2f3ea29ac4d2d28c0bdc153ed967b461c
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-05-30 14:28:57 +0700

    learning useState hook with object

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,32 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+    const [person, setPerson] = useState({
+        name: 'Peter',
+        age: 21,
+        hobby: 'coding addict',
+    });
+
+    const displayPerson = () => {
+        setPerson({
+            name: 'John',
+            age: 28,
+            hobby: 'Screaming At The Computer',
+        });
+
+        // setPerson({ ...person, name: 'John' });
+    };
+
+    return (
+        <>
+            <h3>{person.name}</h3>
+            <h3>{person.age}</h3>
+            <h4>Enjoys: {person.hobby}</h4>
+            <button type="button" onClick={displayPerson} className="btn">
+                Show John
+            </button>
+        </>
+    );
 };
 
 export default UseStateObject;
