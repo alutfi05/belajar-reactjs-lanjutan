4d771ab316dfe17b0155b3f966b3bf214cb8d9b9
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-05-30 11:23:54 +0700

    error example useState hook in react

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,19 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+    let count = 0;
+
+    const handleClickCount = () => {
+        count += 1;
+        console.log(count);
+    };
+
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button type="button" onClick={handleClickCount} className="btn">
+                increase
+            </button>
+        </div>
+    );
 };
 
 export default ErrorExample;
