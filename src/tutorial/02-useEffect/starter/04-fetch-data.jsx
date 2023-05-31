caefa6e50c62c39147d0c613b678d9034d42b14c
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-05-31 15:20:42 +0700

    learning fetching API data using fetch and useEffect hook

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,45 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const result = await response.json();
+
+                // console.log(result);
+                setUsers(result);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <section>
+            <h3>Github users</h3>
+            <ul className="users">
+                {users.map((user) => {
+                    const { avatar_url, login, id, html_url } = user;
+
+                    return (
+                        <li key={id}>
+                            <img src={avatar_url} alt={login} />
+                            <div>
+                                <h5>{login}</h5>
+                                <a href={html_url}>profile</a>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </section>
+    );
 };
 export default FetchData;
