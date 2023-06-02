ad5fdb0982d1f2b92c27a659de6312bed78f7446
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-06-02 14:13:37 +0700

    learning clean up function in useEffect hook

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <div>
+            <button
+                type="button"
+                className="btn"
+                onClick={() => setToggle(!toggle)}
+            >
+                toggle component
+            </button>
+            {toggle && <SecondComponent />}
+        </div>
+    );
+};
+
+const SecondComponent = () => {
+    useEffect(() => {
+        const someFunc = () => {
+            // some logic
+        };
+
+        window.addEventListener('scroll', someFunc);
+
+        return () => {
+            window.removeEventListener('scroll', someFunc);
+        };
+    }, []);
+
+    return <h2>Second Component</h2>;
 };
 
 export default CleanupFunction;
