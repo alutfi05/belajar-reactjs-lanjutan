4a6e10c74628ba2db32d6eea7abe7bc75be9c95b
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-06-02 13:42:33 +0700

    finish ternary user challenge in react js

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+    const [user, setUser] = useState(null);
+
+    const login = () => {
+        setUser({ name: 'lutfi' });
+    };
+
+    const logout = () => {
+        setUser(null);
+    };
+
+    return (
+        <div>
+            {user ? (
+                <div>
+                    <h4>hello there, {user.name}</h4>
+                    <button className="btn" onClick={logout} type="button">
+                        logout
+                    </button>
+                </div>
+            ) : (
+                <div>
+                    <h4>please login</h4>
+                    <button className="btn" onClick={login} type="button">
+                        login
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default UserChallenge;
